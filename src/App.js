75a2fc6084f8b67bbc831d5218f82fc6a3921eaf
Jenn J Will <jennijwill@gmail.com>
2019-05-06 16:28:14 -0700

    checkpoint styling: completed

--- src/App.js
+++ src/App.js
@@ -11,10 +11,14 @@ class App extends Component {
       <div className="App">
         <header>
           <nav>
-            <Link to="/">Landing</Link>
-            <Link to="/library">Library</Link>
+            <Link id="landing" to="/">
+              HOME
+            </Link>
+            <Link id="library" to="/library">
+              MUSIC
+            </Link>
           </nav>
-          <h1>Bloc Jams</h1>
+          <h1>BLOC JAMS</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
